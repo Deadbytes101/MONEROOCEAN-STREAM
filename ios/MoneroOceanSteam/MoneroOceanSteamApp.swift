493cda128665dd801929b1cfd9ca79b61ac62e39
DEADBYTE <termay10000@gmail.com>
2026-06-20 00:03:31 +0700

    birth the phone shell

---
+++ ios/MoneroOceanSteam/MoneroOceanSteamApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct MoneroOceanSteamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
