1c6672cb512d31f86903b4a83f1be51dd382af9f
DEADBYTE <termay10000@gmail.com>
2026-06-20 13:38:30 +0700

    register offline shell

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 import { startApp } from "./src/main.js";
 
 startApp();
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch(() => {});
+  });
+}
